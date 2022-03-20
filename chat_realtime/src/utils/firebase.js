d6dc15b3170aeca267481eb92f6052a714508ece
Sergio Romero <sergio.nicolas777@gmail.com>
2022-03-20 00:22:27 -0300

    feat: add realtime chat project

---
+++ chat_realtime/src/utils/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvnEP30XiX1Xv8ZQIxGL2eqHykXngiSWA",
+  authDomain: "chat-721d5.firebaseapp.com",
+  databaseURL: "https://chat-721d5-default-rtdb.firebaseio.com",
+  projectId: "chat-721d5",
+  storageBucket: "chat-721d5.appspot.com",
+  messagingSenderId: "618628122232",
+  appId: "1:618628122232:web:1f91fc5bcf101fd3f04f8d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
